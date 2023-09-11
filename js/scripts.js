c69e44d8ec08a2044bed4acedc9f0f4ee6bd4474
bphil1041 <benjamin.r.phillips1041@gmail.com>
2023-09-11 08:32:31 -0500

    Update scripts.js
    Added weight to both pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,15 @@ pokemonList = [
     {
         name: 'Bulbasaur',
         type: ['Grass', 'Poison'],
-        height: 0.7
+        height: 0.7,
+        weight: 6.9
     }
     {
         name: 'Abra',
         type: 'Psychic',
-        height: 0.9
+        height: 0.9,
+        weight: 19.5
     }
     
+
 ]
