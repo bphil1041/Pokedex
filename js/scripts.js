33fd9021b2939fd5565025c2d14307d964c86747
bphil1041 <benjamin.r.phillips1041@gmail.com>
2023-09-11 08:36:11 -0500

    Update scripts.js
    added Pikachu information

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,18 @@ pokemonList = [
         weight: 6.9
     }
     {
+        name: 'Pikachu',
+        type: 'Electric',
+        height: 0.4,
+        weight: 6
+    }
+    {
         name: 'Abra',
         type: 'Psychic',
         height: 0.9,
         weight: 19.5
     }
-    
+   
+
 
 ]
