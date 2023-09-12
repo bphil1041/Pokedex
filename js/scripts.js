1b3d1959a7745347b4993381488fb822455e9be1
bphil1041 <benjamin.r.phillips1041@gmail.com>
2023-09-11 23:03:23 -0500

    Update scripts.js
    Print threshold height to DOM

--- js/scripts.js
+++ js/scripts.js
@@ -18,14 +18,22 @@ let pokemonList = [
         height: 1.5,
         weight: 40.5
     }
-]
+];
 
 for (let i=0; i < pokemonList.length; i++){
     //Define pokemonName and pokemonHeight
     let pokemonName = pokemonList[i].name;
     let pokemonHeight = pokemonList[i].height;
+    let thresholdHeight = 1.0;
+    let label = '';
+    
 
+    if (pokemonHeight > thresholdHeight){
+        label = "That's a big pokemon!";
+    }
 
     //Print pokemon name and height to the Dom
-    document.write(pokemonName, pokemonHeight);
+    document.write(<br></br>`${pokemonName} (height: ${pokemonHeight}${label})`);
+
+    
 }
