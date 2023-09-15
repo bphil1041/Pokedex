7e5be41f6109e00b6e953263c2f2c29da4225a34
bphil1041 <benjamin.r.phillips1041@gmail.com>
2023-09-15 08:18:50 -0500

    Comment out forLoop
    (keeping it for now to use as reference)

--- js/scripts.js
+++ js/scripts.js
@@ -23,33 +23,34 @@ let pokemonList = [
 //forEach loop
 pokemonList.forEach(function(pokemon) {
     console.log(pokemon.name);
+    
 });
 
 
-//Iterate the pokemon objects in the array
-for (let i=0; i < pokemonList.length; i++){
-    //Define pokemonName and pokemonHeight
-    let pokemonName = pokemonList[i].name;
-    let pokemonHeight = pokemonList[i].height;
-    let thresholdHeight = 1.0;
-    let label = '';
+// //Iterate the pokemon objects in the array
+// for (let i=0; i < pokemonList.length; i++){
+//     //Define pokemonName and pokemonHeight
+//     let pokemonName = pokemonList[i].name;
+//     let pokemonHeight = pokemonList[i].height;
+//     let thresholdHeight = 1.0;
+//     let label = '';
     
-    //Is pokemon height above threshold?
-    if (pokemonHeight > thresholdHeight){
-        label = " That's a big pokemon!";
-    }
+//     //Is pokemon height above threshold?
+//     if (pokemonHeight > thresholdHeight){
+//         label = " That's a big pokemon!";
+//     }
 
-    //Print pokemon name and height to the Dom
-    document.write(`${pokemonName} (height: ${pokemonHeight}${label})<br>`);
-}
+//     //Print pokemon name and height to the Dom
+//     document.write(`${pokemonName} (height: ${pokemonHeight}${label})<br>`);
+// }
 
-function divide(dividend, divisor){
-    if(divisor === 0){
-        return ("You're trying to divide by Zero")
-    }else {
-            let result = dividend / divisor;
-            return result; 
-        }
+// function divide(dividend, divisor){
+//     if(divisor === 0){
+//         return ("You're trying to divide by Zero")
+//     }else {
+//             let result = dividend / divisor;
+//             return result; 
+//         }
     
 
-}
+// }
