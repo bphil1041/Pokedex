973575eaa82bc4911ac355c67710d17fbae49cbd
bphil1041 <benjamin.r.phillips1041@gmail.com>
2023-09-19 09:48:25 -0500

    Update scripts.js
    format document and add event listener

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,28 @@
-let pokemonRepository = (function (){
+let pokemonRepository = (function () {
 
     let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        type: ['Grass', 'Poison'],
-        height: 0.7,
-        weight: 6.9
-    },
-    {
-        name: 'Pikachu',
-        type: 'Electric',
-        height: 0.4,
-        weight: 6
-    },
-    {
-        name: 'Gengar',
-        type: ['Ghost', 'Poison'],
-        height: 1.5,
-        weight: 40.5
-    }];
+        {
+            name: 'Bulbasaur',
+            type: ['Grass', 'Poison'],
+            height: 0.7,
+            weight: 6.9
+        },
+        {
+            name: 'Pikachu',
+            type: 'Electric',
+            height: 0.4,
+            weight: 6
+        },
+        {
+            name: 'Gengar',
+            type: ['Ghost', 'Poison'],
+            height: 1.5,
+            weight: 40.5
+        }];
+
+    function showDetails(pokemon) {
+        console.log(pokemon);
+    }
 
     function addListItem(pokemon) {
         let listItem = document.createElement('li');
@@ -27,14 +31,17 @@ let pokemonRepository = (function (){
         button.classList.add('pokemon-button');
         listItem.appendChild(button);
         ulElement.appendChild(listItem);
+        button.addEventListener('click', function () {
+            showDetails(pokemon);
+        });
     }
 
 
     return {
-        getAll: function() {
+        getAll: function () {
             return pokemonList;
         },
-        add: function(item){
+        add: function (item) {
             if (typeof item === 'object' && 'name' in item) {
                 pokemonList.push(item);
             } else {
@@ -45,8 +52,13 @@ let pokemonRepository = (function (){
     };
 })();
 
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: "Pikachu", type: ["Electric"], height: 0.3, weight: 13 });
+
+console.log(pokemonRepository.getAll());
+
 let ulElement = document.querySelector('.pokemon-list');
 //forEach loop
-pokemonRepository.getAll().forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
 });
