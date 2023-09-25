449e8fe16b8dd5b2b689314f745adad3f6ed547a
bphil1041 <benjamin.r.phillips1041@gmail.com>
2023-09-25 08:24:02 -0500

    add loadDetails function script.js

--- js/scripts.js
+++ js/scripts.js
@@ -48,4 +48,17 @@ let pokemonRepository = (function () {
         })
     }
 
-    
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function(response){
+            return response.json();
+        }).then(function (details) {
+            item.imageUrl = details.sprites.front_default;
+            item.height = details.height;
+            item.types = details.types;
+        }).catch(function(e) {
+            console.error(e);
+        });
+    }
+
+   
